47e8812f5cf09eb89b7bc22f5bd1a3ff5833e25d
gio <giozedelashvili@gmail.com>
2024-11-11 21:14:53 +0400

    first commit

--- src/components/ThankYouMessage/ThankYouMessage.jsx
+++ src/components/ThankYouMessage/ThankYouMessage.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './ThankYouMessage.css';
 
-function ThankYouMessage({ selectedRating, onComeback }) {
+const ThankYouMessage = ({ selectedRating, onGoBack }) => {
   return (
     <div className="thank-you-message">
-      
-      <p className="selected-rating">You selected {selectedRating} out of 5</p>
+      <img 
+        src="https://media3.giphy.com/media/fxI1G5PNC5esyNlIUs/giphy.gif" 
+        alt="Thank you" 
+        className="illustration" 
+      />
+      <div className="selected-rating">You selected {selectedRating} out of 5</div>
       <h3>Thank you!</h3>
-      <p>We appreciate your feedback and are glad to help you.</p>
-    
-      <button onClick={onComeback} className="comeback-button">
-        Comeback
-      </button>
+      <p>We are glad to help you.</p>
+      
     </div>
   );
-}
+};
 
 export default ThankYouMessage;
